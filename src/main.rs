a0a38f7df1ffb3251008f39cddb07429d0aad0b5
Richard Lopes <richard.lopes1995@gmail.com>
2024-10-24 00:14:11 -0300

    Implement decode of lists

--- src/main.rs
+++ src/main.rs
@@ -1,92 +1,13 @@
 use serde_json::{self};
-use std::{env, ops::Add};
+use std::{env};
 
 // Available if you need it!
 // use serde_bencode
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    //println!("{}", encoded_value);
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        // Example: "i43e" -> 43
-        let first_char = &encoded_value[0..1];
-
-        return match first_char {
-            "i" => {
-                let last_char = &encoded_value.find("e");
-                if last_char.is_none() {
-                    panic!("Need e");
-                }
-                let number_string = &encoded_value[1..last_char.unwrap()];
-                serde_json::Value::Number(number_string.parse::<i64>().unwrap().into())
-            }
-            "l" => {
-                let mut values = Vec::<serde_json::Value>::new();
-                let mut pivot = 1;
-                let mut end_pivot = encoded_value.len() - 1;
-
-                while pivot < end_pivot {
-                    let all_values = &encoded_value[pivot..end_pivot];
-                    let decoded_value = decode_bencoded_value(all_values);
-                    let (aux_pivot, aux_end_pivot) = find_pivot(decoded_value.clone());
-                    pivot = pivot + aux_pivot;
-                    end_pivot = end_pivot - aux_end_pivot;
-                    values.push(decoded_value);
-                }
-
-                serde_json::Value::Array(values)
-            }
-            _ => panic!("Not implemented type"),
-        };
-    }
-}
-
-fn find_pivot(v: serde_json::Value) -> (usize, usize) {
-    if v.is_string() {
-        let str_size = v.as_str().unwrap().len();
-        let digit_size = str_size.to_string().len();
-        //println!("DEBUG - v: {:?} - size {}",v, str_size + digit_size);
-        return (v.as_str().unwrap().len().add(1 + digit_size), 0);
-    }
-
-    if v.is_i64() {
-        return (v.as_i64().map(|x| x.to_string()).unwrap().len().add(2), 0);
-    }
-
-    if v.is_array() {
-        let a = v.as_array().map(|a| {
-            let b = a
-                .iter()
-                .map(|x| {
-                    let z = find_pivot(x.clone());
-                    z
-                })
-                .collect::<Vec<(usize, usize)>>();
-            if b.len() == 0 {
-                return (1, 1)
-            }
-            //println!("DEBUG - {:?} - b {:?}", v, b);
-            let mut aux_a = 0;
-            let mut aux_b = 0;
-            for ele in b {
-                aux_a += ele.0 + 1;
-                aux_b += ele.1 + 1;
-            }
-            (aux_a, aux_b)
-        });
-        //println!("DEBUG - {:?}", a);
-        return a.unwrap();
-    }
-
-    (0, 0)
+    let x = serde_bencode::de::from_str::<serde_json::Value>(encoded_value);
+    x.unwrap()
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
